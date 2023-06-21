5ce2526d9ef3c7fa04759bce862ba776f5c774d7
Alejandro Salazar <alejandroslzr17@gmail.com>
2023-06-20 21:48:08 -0600

    fixed link for styles css

--- controllers/dashRoutes.js
+++ controllers/dashRoutes.js
@@ -4,4 +4,22 @@ const router = require('express').Router();
 
 // CRUD Operations
 
+router.get('/', async (req,res)=>{   
+    try{
+        res.render('homepage')
+    } catch(err){
+        res.status(400).json(err)
+    }
+})
+
+
+router.get('/login', async (req,res)=>{   
+    try{
+        res.render('login');
+    } catch(err){
+        res.status(400).json(err)
+    }
+})
+
+
 module.exports = router;
