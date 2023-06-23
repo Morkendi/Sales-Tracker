d9262da7209ad1745bd9d631aeff74de380db6b4
Alejandro Salazar <alejandroslzr17@gmail.com>
2023-06-22 20:35:06 -0600

    erased comments on models/index

--- models/index.js
+++ models/index.js
@@ -1,34 +1,29 @@
+
 const User = require('./User');
 const Sale = require('./Sale');
 const Client = require('./Client');
 const Product = require('./Product');
+const SaleProduct = require('./SaleProduct');
 
 User.hasMany(Sale, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
 });
-
 Sale.belongsTo(User, {
   foreignKey: 'user_id'
 });
-
 Sale.belongsTo(Client, {
   foreignKey: 'client_id'
 });
-
 Sale.belongsToMany(Product, {
   through: 'SaleProduct',
   foreignKey: 'sale_id'
 });
-
 Product.belongsToMany(Sale, {
   through: 'SaleProduct',
   foreignKey: 'product_id'
 });
-
 Client.hasMany(Sale, {
   foreignKey: 'client_id',
-  onDelete: 'CASCADE'
 });
-
-module.exports = { User, Sale, Client, Product };
+module.exports = { User, Sale, Client, Product, SaleProduct };
