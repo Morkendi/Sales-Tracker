5c7b5a568679fadf5c51ee4f0cff29d90ddc5397
Daniel Sanchez <morkendi@gmail.com>
2023-06-22 18:36:51 -0600

    User & Sale routes

--- controllers/api/saleRoutes.js
+++ controllers/api/saleRoutes.js
@@ -1,7 +1,77 @@
 const router = require('express').Router();
 // Require Model
-// Require heper functions
+const { Sale } = require('../../models');
 
 // CRUD Operations
+
+// Get All
+router.get('/', async (req, res) => {
+    try{
+        const saleData = await Sale.findAll(
+            // TODO: Add associated models (If any)
+        );
+        saleData
+            ? res.status(200).json(saleData)
+            : res.status(404).json({ message: "No sales found!" });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// Get by ID
+router.get('/:id', async (req, res) => {
+    try{
+        const singleSale = await Sale.findByPk(req.params.id, {
+            // TODO: Add associated models (If any)
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// Create Sale
+router.post('/', async (req, res) => {
+    try{
+        const newSale = await Sale.create({
+            user_id: req.body.user_id,
+            client_id: req.body.client_id,
+            date_created: req.body.date_created
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// Update Sale
+router.put('/:id', async (req, res) => {
+    try{
+        const updateSale = await Sale.update(req.body, {
+            where: {
+                id: req.params.id
+            }
+        });
+        updateSale
+            ? res.status(200).json(updateSale)
+            : res.status(404).json({ message: "No sale found!" });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// Delete Sale
+router.delete('/:id', async (req, res) => {
+    try{
+        const deleteSale = await Sale.destroy({
+            where: {
+                id: req.params.id
+            }
+        });
+        deleteSale
+            ? res.status(200).json(deleteSale)
+            : res.status(404).json({ message: "No sale found!" });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,77 +1,57 @@
 const router = require('express').Router();
-const {User} = require('../../models')
+// Require Model
+const { User } = require('../../models')
 
-  
+// CRUD Operations
 
-router.post('/signup', async (req, res) => {
+// User Login
+router.post('/login', async (req, res) => {
     try {
-      const dbUserData = await User.create({
-        email: req.body.email,
-        name: req.body.name,
-        password: req.body.password,
-      });
-  
-      req.session.save(() => {
-        req.session.user_id = dbUserData.id;
-        req.session.loggedIn = true;
-  
-        res.status(200).json(dbUserData);
-      });
-      
-    } catch (err) {
-      console.log(err);
-      res.status(500).json(err);
-    }
-  });
-
-  router.post('/login', async (req, res) => {
-    try {
-      const dbUserData = await User.findOne({
+    const dbUserData = await User.findOne({
         where: {
-          email: req.body.email,
+        username: req.body.email,
         },
-      });
-  
-      if (!dbUserData) {
+    });
+
+    if (!dbUserData) {
         res
-          .status(400)
-          .json({ message: 'Incorrect email or password. Please try again!' });
+        .status(400)
+        .json({ message: 'Incorrect email or password. Please try again!' });
         return;
-      }
-  
-      const validPassword = await dbUserData.checkPassword(req.body.password);
-  
-      if (!validPassword) {
+    }
+
+    const validPassword = await dbUserData.checkPassword(req.body.password);
+
+    if (!validPassword) {
         res
-          .status(400)
-          .json({ message: 'Incorrect email or password. Please try again!' });
+        .status(400)
+        .json({ message: 'Incorrect email or password. Please try again!' });
         return;
-      }
-  
-      req.session.save(() => {
+    }
+
+    req.session.save(() => {
         req.session.user_id = dbUserData.id;
         req.session.loggedIn = true;
-  
+
         res.status(200).json({ user: dbUserData, message: 'You are now logged in!' });
-      });
-     
+    });
+
     } catch (err) {
-      console.log(err);
-      res.status(500).json(err);
+    console.log(err);
+    res.status(500).json(err);
     }
-  });
-  
-  // Logout
-  router.post('/logout', (req, res) => {
+});
+
+// User Logout
+router.post('/logout', (req, res) => {
     if (req.session.loggedIn) {
-      req.session.destroy(() => {
+    req.session.destroy(() => {
         res.status(204).end();
-      });
+    });
     } else {
-      res.status(404).end();
+    res.status(404).end();
     }
-  });
-  
-  module.exports = router;
+});
+
 
 module.exports = router;
