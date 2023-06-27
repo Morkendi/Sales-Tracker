a1bfc8cac4c7f48f48284a0c54bded3cd0d3e008
carloscastilloflores <125534814+carloscastilloflores@users.noreply.github.com>
2023-06-26 18:52:35 -0600

    Revert "Updated Dashbaord with new layout first"
    This reverts commit 8e037a7fb239729267381ef8023994e1d10507fa.

--- routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = require('express').Router();
-const apiRoutes = require('./api');
-const database = require ('../sales_db')
-router.use('/api', apiRoutes);
-
-
-module.exports = router;
